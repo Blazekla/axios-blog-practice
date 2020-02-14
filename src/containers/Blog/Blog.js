800363ebc41a9ec10e989a4131d923b88037c034
Blazekla <cristlui4@gmail.com>
2020-02-14 17:34:32 -0600

    added axios interceptors code

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   };
 
   componentDidMount() {
@@ -24,6 +25,11 @@ class Blog extends Component {
           };
         });
         this.setState({ posts: updatedPosts });
+      })
+      .catch(err => {
+        //cancelToken error due to log below when used with interceptor
+        console.log(err);
+        this.setState({ error: true });
       });
   }
 
@@ -32,18 +38,21 @@ class Blog extends Component {
   };
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          title={post.title}
-          key={post.id}
-          author={post.author}
-          //Either of the props below will pass the event handler as reference with a param
-          //   clicked={() => this.postSelectedHandler(post.id)}
-          clicked={this.postSelectedHandler.bind(this, post.id)}
-        />
-      );
-    });
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            title={post.title}
+            key={post.id}
+            author={post.author}
+            //Either of the props below will pass the event handler as reference with a param
+            //   clicked={() => this.postSelectedHandler(post.id)}
+            clicked={this.postSelectedHandler.bind(this, post.id)}
+          />
+        );
+      });
+    }
 
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+//Request interceptor is a common case to add common headers i.e. authorization
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+
+    //Edit request config before returning
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    console.log(response);
+
+    //Edit request config before returning
+    return response;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
