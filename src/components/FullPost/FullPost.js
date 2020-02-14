635ea8f49459ff93a9e8f45cd788d469b9056f46
Blazekla <cristlui4@gmail.com>
2020-02-14 12:18:03 -0600

    refact code from personal solution to tutorial code

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,17 +4,19 @@ import "./FullPost.css";
 
 class FullPost extends Component {
   render() {
-    let post = this.props.selected ? (
-      <div className="FullPost">
-        <h1>{this.props.all[0].title}</h1>
-        <p>{this.props.all[0].body}</p>
-        <div className="Edit">
-          <button className="Delete">Delete</button>
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>Title</h1>
+          <p>Content</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
         </div>
-      </div>
-    ) : (
-      <p style={{ textAlign: "center" }}>Please select a Post!</p>
-    );
+      );
+    }
+
     return post;
   }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,12 +9,10 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
-    selectedId: null,
-    resultPost: null
+    selectedPostId: null
   };
 
   componentDidMount() {
-    console.log("prior to axios call");
     axios
       .get("https://jsonplaceholder.typicode.com/posts?_limit=5")
       .then(response => {
@@ -26,29 +24,13 @@ class Blog extends Component {
           };
         });
         this.setState({ posts: updatedPosts });
-        console.log("response value from component did Mount: ");
-        console.log(response);
       });
   }
 
-  selectedPost = id => {
-    this.setState({ selectedId: id });
-    console.log(`selected post id on clicked: ${id}`);
-    console.log("log prior to resultPost call");
-    this.resultPost(id);
-    console.log(`selectedID after resultPost call: ${this.state.selectedId}`);
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
   };
 
-  //test post function
-  resultPost = id => {
-    let result = this.state.posts.filter(obj => {
-      console.log(this.state.selectedId);
-      return obj.id === id;
-    });
-    console.log("resultPost func result here:");
-    console.log(result);
-    this.setState({ resultPost: result });
-  };
   render() {
     const posts = this.state.posts.map(post => {
       return (
@@ -57,31 +39,17 @@ class Blog extends Component {
           key={post.id}
           author={post.author}
           //Either of the props below will pass the event handler as reference with a param
-          //   clicked={() => this.selectedPost(post.id)}
-          clicked={this.selectedPost.bind(this, post.id)}
+          //   clicked={() => this.postSelectedHandler(post.id)}
+          clicked={this.postSelectedHandler.bind(this, post.id)}
         />
       );
     });
-    // console.log(`posts value: ${posts[0]}`);
-    // console.log(posts);
-    // let result = posts.filter(obj => {
-    //   console.log(
-    //     `The object key: ${obj} & selectedId: ${this.state.selectedId}`
-    //   );
-    //   return obj.key === this.state.selectedId;
-    // });
+
     return (
       <div>
-        {
-          (console.log("Render result value: "),
-          console.log(this.state.resultPost))
-        }
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost
-            selected={this.state.selectedId}
-            all={this.state.resultPost}
-          />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
