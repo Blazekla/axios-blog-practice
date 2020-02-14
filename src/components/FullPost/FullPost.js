233370fa3db9da0dbe610b7872afeb68b0b83efd
Blazekla <cristlui4@gmail.com>
2020-02-14 11:49:46 -0600

    added code that displayed clicked post with workaround for state management

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-
-        );
-        return post;
-    }
+  render() {
+    let post = this.props.selected ? (
+      <div className="FullPost">
+        <h1>{this.props.all[0].title}</h1>
+        <p>{this.props.all[0].body}</p>
+        <div className="Edit">
+          <button className="Delete">Delete</button>
+        </div>
+      </div>
+    ) : (
+      <p style={{ textAlign: "center" }}>Please select a Post!</p>
+    );
+    return post;
+  }
 }
 
 export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,32 +8,80 @@ import "./Blog.css";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedId: null,
+    resultPost: null
   };
 
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: "Max"
-        };
+    console.log("prior to axios call");
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts?_limit=5")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        console.log("response value from component did Mount: ");
+        console.log(response);
       });
-      this.setState({ posts: updatedPosts });
-      console.log(response);
-    });
   }
 
+  selectedPost = id => {
+    this.setState({ selectedId: id });
+    console.log(`selected post id on clicked: ${id}`);
+    console.log("log prior to resultPost call");
+    this.resultPost(id);
+    console.log(`selectedID after resultPost call: ${this.state.selectedId}`);
+  };
+
+  //test post function
+  resultPost = id => {
+    let result = this.state.posts.filter(obj => {
+      console.log(this.state.selectedId);
+      return obj.id === id;
+    });
+    console.log("resultPost func result here:");
+    console.log(result);
+    this.setState({ resultPost: result });
+  };
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post title={post.title} key={post.id} author={post.author} />;
+      return (
+        <Post
+          title={post.title}
+          key={post.id}
+          author={post.author}
+          //Either of the props below will pass the event handler as reference with a param
+          //   clicked={() => this.selectedPost(post.id)}
+          clicked={this.selectedPost.bind(this, post.id)}
+        />
+      );
     });
+    // console.log(`posts value: ${posts[0]}`);
+    // console.log(posts);
+    // let result = posts.filter(obj => {
+    //   console.log(
+    //     `The object key: ${obj} & selectedId: ${this.state.selectedId}`
+    //   );
+    //   return obj.key === this.state.selectedId;
+    // });
     return (
       <div>
+        {
+          (console.log("Render result value: "),
+          console.log(this.state.resultPost))
+        }
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost
+            selected={this.state.selectedId}
+            all={this.state.resultPost}
+          />
         </section>
         <section>
           <NewPost />
