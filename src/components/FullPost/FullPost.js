c07fe8c2eb247d77b36c6327a1fb5fdc1585b92f
Blazekla <cristlui4@gmail.com>
2020-02-15 14:21:52 -0600

    added default global axios configuration code

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -13,22 +13,18 @@ class FullPost extends Component {
         !this.state.loadedPost ||
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
       ) {
-        axios
-          .get("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-          .then(response => {
-            console.log(response);
-            this.setState({ loadedPost: response.data });
-          });
+        axios.get("/posts/" + this.props.id).then(response => {
+          console.log(response);
+          this.setState({ loadedPost: response.data });
+        });
       }
     }
   }
 
   deletePostHandler = () => {
-    axios
-      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-      .then(response => {
-        console.log(response);
-      });
+    axios.delete("/posts/" + this.props.id).then(response => {
+      console.log(response);
+    });
   };
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,7 +15,7 @@ class Blog extends Component {
 
   componentDidMount() {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts?_limit=5")
+      .get("/posts?_limit=5")
       .then(response => {
         const posts = response.data.slice(0, 4);
         const updatedPosts = posts.map(post => {
